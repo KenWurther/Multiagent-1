46b6c15123685738602edd7a8869540f960e222d
Chidambaram Ramanathan <chidambaram.ramanath@gmail.com>
2014-10-16 23:18:50 -0400

    final version

--- multiAgents.py
+++ multiAgents.py
@@ -71,55 +71,51 @@ class ReflexAgent(Agent):
         # Useful information you can extract from a GameState (pacman.py)
         returnScore = float(0)
         successorGameState = currentGameState.generatePacmanSuccessor(action)
-        oldPos = currentGameState.getPacmanPosition()
+        
+	oldPos = currentGameState.getPacmanPosition()
         newPos = successorGameState.getPacmanPosition()
-        newFood = successorGameState.getFood()
+        
+	newFood = successorGameState.getFood()
         oldFood = currentGameState.getFood()
-        oldGhostStates = currentGameState.getGhostStates()
+        
+	oldGhostStates = currentGameState.getGhostStates()
         newGhostStates = successorGameState.getGhostStates()
-        newScaredTimes = [ghostState.scaredTimer for ghostState in newGhostStates]
-        #sorted distance of the ghost
-        sortedGhostDist = 999999
-        sortedGhostPos = 0
+        
+        minGhostDist = 999999
+        minGhostPos = 0
+
         # Calculate new manhattan distance between pacman and ghost
         newGhostPositions=[ghost.getPosition() for ghost in newGhostStates]
         #sorting ghost positions based on nearness.
-        i = 0
-        for ghost in newGhostPositions:
-            if manhattanDistance(newPos, ghost) < sortedGhostDist:
-               sortedGhostDist = manhattanDistance(newPos, ghost)
-               sortedGhostPos = i
-            i = i + 1
-        ghostFactor = 0
-        #Suppose no ghosts in grid
-        if len(newGhostPositions) == 0: 
-            ghostFactor = 0
-        #Pacman Dies
-        if manhattanDistance(newPos, newGhostPositions[sortedGhostPos]) == 0:
-            return -99
-        else:#The scores are always negative for ghosts, magnify the reciprocal as suggested in the PDF.
-            ghostFactor = 5 * (-1.0/manhattanDistance(newPos, newGhostPositions[sortedGhostPos]))
+        for i,ghost in enumerate(newGhostPositions):
+	    # If ghost can eat pacman, dont go there
+	    if ghost == newPos:
+		return -999999
+            if manhattanDistance(newPos, ghost) < minGhostDist:
+               minGhostDist = manhattanDistance(newPos, ghost)
+               minGhostPos = i
+       
+	# Determines the influence of ghost on Pacman 
+        ghostFactor = 5 * (-1.0/manhattanDistance(newPos, newGhostPositions[minGhostPos]))
         
         #Sorted food poistions based on nearness
-        sortedFoodDist = 999999
-        sortedFoodPos = 0
-        i = 0
+        minFoodDist = 999999
+        minFoodPos = 0
         foodPositions = oldFood.asList()
-        for food in foodPositions:
-            if manhattanDistance(newPos, food) < sortedFoodDist:
-               sortedFoodDist = manhattanDistance(newPos, food)
-               sortedFoodPos = i
-            i = i + 1
-        #food scores are always positive get the nearest distance for the food.
-        foodFactor = manhattanDistance(newPos, foodPositions[sortedFoodPos])
+
+	# Find the closest food distance
+        for i,food in enumerate(foodPositions):
+            if manhattanDistance(newPos, food) < minFoodDist:
+               minFoodDist = manhattanDistance(newPos, food)
+               minFoodPos = i
+
+        # Determines the influence of food on Pacman
+        foodFactor = manhattanDistance(newPos, foodPositions[minFoodPos])
         
         # Score calculation (food score may be zero, so check otherwise just take reciprocal as suggested in the pdf.
-        if foodFactor == 0:
-           returnScore = 5.0 + ghostFactor
-        else:
-           returnScore = ghostFactor + 1.0 / float(foodFactor)
+	return ((5.0 + ghostFactor) if foodFactor == 0 else (ghostFactor + 1.0/float(foodFactor)))
 
-        return returnScore
+        #return returnScore
         "*** YOUR CODE HERE ***"
 
 def scoreEvaluationFunction(currentGameState):
@@ -191,15 +187,11 @@ class MinimaxAgent(MultiAgentSearchAgent):
         and self.evaluationFunction.
         """
         "*** YOUR CODE HERE ***"
-        # Total number of agents
-        agents = gameState.getNumAgents()
-        # depth
-        d = self.depth*agents
-	    # Get actions and remove STOP if present
         actions = gameState.getLegalActions(0)
         if Directions.STOP in actions:
             actions.remove(Directions.STOP)
-        val = [self.MiniMax(gameState.generateSuccessor(0, action), d -1 , 1, agents) for action in actions]
+        val = [self.MiniMax(gameState.generateSuccessor(0, action), (self.depth*gameState.getNumAgents()) -1 , 1, 
+			gameState.getNumAgents()) for action in actions]
         bestval = max(val)
         indices = [index for index in range(len(val)) if val[index] == bestval]
         return actions[random.choice(indices)]
@@ -245,16 +237,15 @@ class AlphaBetaAgent(MultiAgentSearchAgent):
         Returns the minimax action using self.depth and self.evaluationFunction
         """
         "*** YOUR CODE HERE ***"
-        numOfAgent = gameState.getNumAgents()
-        legalMoves = gameState.getLegalActions(0)
+        actions = gameState.getLegalActions(0)
         #calculate depth based on the number of agents (the tree has one agent below the other)
-        trueDepth = self.depth * numOfAgent
+        depth = self.depth * gameState.getNumAgents()
         alpha = float('-Inf')
         beta = float('Inf')
         scores = []
         #Calling min agents from the root node (considering root node as max agent)
-        for action in legalMoves:
-           score = self.AlphaBeta(gameState.generateSuccessor(0, action), trueDepth-1, 1, numOfAgent, alpha, beta)
+        for action in actions:
+           score = self.AlphaBeta(gameState.generateSuccessor(0, action), depth-1, 1, gameState.getNumAgents(), alpha, beta)
            scores.append(score)
            #be sure to update alpha in the root (max agent)
            alpha = max(alpha, score)
@@ -262,7 +253,7 @@ class AlphaBetaAgent(MultiAgentSearchAgent):
         bestIndices = [index for index in range(len(scores)) if scores[index] == bestScore]
         chosenIndex = random.choice(bestIndices)
         #Return the best move by selecting a random action from multiple best choices (if any)
-        return legalMoves[chosenIndex]
+        return actions[chosenIndex]
 
 class ExpectimaxAgent(MultiAgentSearchAgent):
     """
